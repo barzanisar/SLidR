af55cac855522294d58f0dff558c7ae03d5341bb
Barza Nisar <barzanisar93@gmail.com>
2024-09-19 17:07:40 -0400

    wandb and directories setup

--- pretrain.py
+++ pretrain.py
@@ -1,14 +1,22 @@
 import os
 import argparse
+import logging
+from pathlib import Path
+import wandb
 import torch.nn as nn
 import MinkowskiEngine as ME
 import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
 from utils.read_config import generate_config
+from utils.wandb_utils import init_or_resume_wandb_run
 from pretrain.model_builder import make_model
 from pytorch_lightning.plugins import DDPPlugin
+from pytorch_lightning.strategies import DDPStrategy
 from pretrain.lightning_trainer import LightningPretrain
 from pretrain.lightning_datamodule import PretrainDataModule
 from pretrain.lightning_trainer_spconv import LightningPretrainSpconv
+from pytorch_lightning.callbacks import ModelCheckpoint
+
 
 
 def main():
@@ -22,15 +30,57 @@ def main():
     parser.add_argument(
         "--resume_path", type=str, default=None, help="provide a path to resume an incomplete training"
     )
+    parser.add_argument(
+        "--num_gpus", type=int, default=1, help="num gpus"
+    )
+    parser.add_argument(
+        "--extra_tag", type=str, default=None, help="pretrain extra tag"
+    )
     args = parser.parse_args()
     config = generate_config(args.cfg_file)
-    if args.resume_path:
-        config['resume_path'] = args.resume_path
+
+    if args.extra_tag:
+        config['extra_tag'] = args.extra_tag
+    if args.num_gpus > 1:
+         config['num_gpus'] = args.num_gpus
+    
 
     if os.environ.get("LOCAL_RANK", 0) == 0:
         print(
             "\n" + "\n".join(list(map(lambda x: f"{x[0]:20}: {x[1]}", config.items())))
         )
+
+    logging.info(f"Creating output directories: ")
+    savedir_root = os.path.join(config["save_dir"], "Pretraining", config['desc'], config["extra_tag"])
+    if os.environ.get("LOCAL_RANK", 0) == 0:
+        os.makedirs(savedir_root, exist_ok=True)
+    checkpoints_dir = os.path.join(savedir_root, 'checkpoints')
+    config['savedir_root'] = savedir_root
+    logging.info(f"Savedir_root: {savedir_root}")
+    logging.info(f"checkpoints_dir: {checkpoints_dir}")
+    
+    wandb_logger = None
+    if config.get("wandb", {}).get("enabled"):
+        run_id_file = Path(savedir_root)/ 'wandb_run_id.txt'
+        config['wandb']['group'] = config['extra_tag']
+        config['wandb']['job_type'] = 'pretrain'
+        init_or_resume_wandb_run(run_id_file, config)
+        wandb_logger = pl_loggers.WandbLogger(experiment=wandb.run)
+        wandb_logger.log_dir = savedir_root
+
+    if args.resume_path:
+        resume_path = args.resume_path
+    elif config["resume_path"]:
+        # Resume either from config or last saved checkpoint
+        resume_path = config["resume_path"]
+    else:
+        resume_path = os.path.join(checkpoints_dir, 'last.ckpt')
+        if not os.path.isfile(resume_path):
+            resume_path=None
+    
+    config["resume_path"] = resume_path
+    logging.info(f"Ckpt resume_path: {resume_path}")
+
 
     dm = PretrainDataModule(config)
     model_points, model_images = make_model(config)
@@ -41,20 +91,33 @@ def main():
         module = LightningPretrain(model_points, model_images, config)
     elif config["model_points"] == "voxelnet":
         module = LightningPretrainSpconv(model_points, model_images, config)
-    path = os.path.join(config["working_dir"], config["datetime"])
+    #path = os.path.join(config["working_dir"], config["datetime"])
+    # ckpt_path=os.path.join(path, 'ckpts')
+
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=checkpoints_dir,
+        filename="{epoch:02d}",
+        every_n_epochs=0,
+        save_top_k=-1,
+        save_last=True
+    )
     trainer = pl.Trainer(
         gpus=config["num_gpus"],
-        accelerator="ddp",
-        default_root_dir=path,
-        checkpoint_callback=True,
-        max_epochs=config["num_epochs"],
-        plugins=DDPPlugin(find_unused_parameters=False),
+        accelerator="cuda",
+        default_root_dir=savedir_root,
+        callbacks=[checkpoint_callback],
+        strategy=DDPStrategy(find_unused_parameters=False),
         num_sanity_val_steps=0,
-        resume_from_checkpoint=config["resume_path"],
+        resume_from_checkpoint=resume_path,
+        logger=wandb_logger,
+        max_epochs=25,
         check_val_every_n_epoch=1,
+        log_every_n_steps=100
+
     )
     print("Starting the training")
-    trainer.fit(module, dm)
+    trainer.fit(module, dm) #, ckpt_path=resume_path
 
 
 if __name__ == "__main__":
--- pretrain/lightning_trainer.py
+++ pretrain/lightning_trainer.py
@@ -22,15 +22,15 @@ class LightningPretrain(pl.LightningModule):
         self.batch_size = config["batch_size"]
         self.num_epochs = config["num_epochs"]
         self.superpixel_size = config["superpixel_size"]
-        self.epoch = 0
-        if config["resume_path"] is not None:
-            self.epoch = int(
-                re.search(r"(?<=epoch=)[0-9]+", config["resume_path"])[0]
-            )
+        # self.epoch = 0
+        # if config["resume_path"] is not None:
+        #     self.epoch = int(
+        #         re.search(r"(?<=epoch=)[0-9]+", config["resume_path"])[0]
+        #     )
         self.criterion = NCELoss(temperature=config["NCE_temperature"])
-        self.working_dir = os.path.join(config["working_dir"], config["datetime"])
-        if os.environ.get("LOCAL_RANK", 0) == 0:
-            os.makedirs(self.working_dir, exist_ok=True)
+        # self.working_dir = os.path.join(config["working_dir"], config["datetime"])
+        # if os.environ.get("LOCAL_RANK", 0) == 0:
+        #     os.makedirs(self.working_dir, exist_ok=True)
 
     def configure_optimizers(self):
         optimizer = optim.SGD(
@@ -48,10 +48,10 @@ class LightningPretrain(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         sparse_input = ME.SparseTensor(batch["sinput_F"], batch["sinput_C"])
-        output_points = self.model_points(sparse_input).F
+        output_points = self.model_points(sparse_input).F #(num voxels in batch, C=64)
         self.model_images.eval()
         self.model_images.decoder.train()
-        output_images = self.model_images(batch["input_I"])
+        output_images = self.model_images(batch["input_I"]) #(6 views*4 bs=24, C=64, H=224, W=416)
 
         del batch["sinput_F"]
         del batch["sinput_C"]
@@ -74,32 +74,33 @@ class LightningPretrain(pl.LightningModule):
         pairing_points = batch["pairing_points"]
         pairing_images = batch["pairing_images"]
         idx = np.random.choice(pairing_points.shape[0], self.num_matches, replace=False)
-        k = output_points[pairing_points[idx]]
+        k = output_points[pairing_points[idx]] #[num pts=4096, 64] pt features
         m = tuple(pairing_images[idx].T.long())
-        q = output_images.permute(0, 2, 3, 1)[m]
+        q = output_images.permute(0, 2, 3, 1)[m] #[num pixels=4096, 64] corresponding img pixel features
         return self.criterion(k, q)
 
     def loss_superpixels_average(self, batch, output_points, output_images):
         # compute a superpoints to superpixels loss using superpixels
+        # computes avg features for points and pixels for each segment (i.e. computes superpoint and superpixel features and matching/pairing point and img features)
         torch.cuda.empty_cache()  # This method is extremely memory intensive
-        superpixels = batch["superpixels"]
-        pairing_images = batch["pairing_images"]
-        pairing_points = batch["pairing_points"]
+        superpixels = batch["superpixels"] #(24, 224, 416) slic segment ids
+        pairing_images = batch["pairing_images"] #(num pairing pts in batch, 3= [0 to 23 image id, v pixel, u pixel])
+        pairing_points = batch["pairing_points"] #(num pairing pts in batch, inverse_indices)
 
         superpixels = (
             torch.arange(
                 0,
-                output_images.shape[0] * self.superpixel_size,
-                self.superpixel_size,
+                output_images.shape[0] * self.superpixel_size, #24 images * 150 superpixels
+                self.superpixel_size, # 150
                 device=self.device,
-            )[:, None, None] + superpixels
-        )
-        m = tuple(pairing_images.cpu().T.long())
+            )[:, None, None] + superpixels # (24,1,1) [0, 150, 300, 450, ...] + (24, 224, 416) slic segment ids
+        ) # to increase segment ids for each img by 150
+        m = tuple(pairing_images.cpu().T.long()) #tuple: 0: img ids, 1: v pixels, 2: u pixels
 
-        superpixels_I = superpixels.flatten()
-        idx_P = torch.arange(pairing_points.shape[0], device=superpixels.device)
-        total_pixels = superpixels_I.shape[0]
-        idx_I = torch.arange(total_pixels, device=superpixels.device)
+        superpixels_I = superpixels.flatten() #(24, H, W) -> (24*H*W)
+        idx_P = torch.arange(pairing_points.shape[0], device=superpixels.device) # 0, ..., num pairing pts -1  
+        total_pixels = superpixels_I.shape[0] # num pixels in all 24 imgs
+        idx_I = torch.arange(total_pixels, device=superpixels.device) # 0, ..., num pixels-1
 
         with torch.no_grad():
             one_hot_P = torch.sparse_coo_tensor(
@@ -108,7 +109,7 @@ class LightningPretrain(pl.LightningModule):
                 ), dim=0),
                 torch.ones(pairing_points.shape[0], device=superpixels.device),
                 (superpixels.shape[0] * self.superpixel_size, pairing_points.shape[0])
-            )
+            ) # sparse one hot matrix of size: (24 imgs *150 max segments, num FOV points for all 24 imgs) where first input is (row indices: segment/superpixel ids of pairing pts, col indices: torch arange num pairing pts), second input is values i.e. ones to put in those row and col indices
 
             one_hot_I = torch.sparse_coo_tensor(
                 torch.stack((
@@ -116,22 +117,31 @@ class LightningPretrain(pl.LightningModule):
                 ), dim=0),
                 torch.ones(total_pixels, device=superpixels.device),
                 (superpixels.shape[0] * self.superpixel_size, total_pixels)
-            )
+            ) # sparse one hot matrix of size: (24 imgs *150 max segments, total pixels in all 24 imgs)
 
-        k = one_hot_P @ output_points[pairing_points]
-        k = k / (torch.sparse.sum(one_hot_P, 1).to_dense()[:, None] + 1e-6)
-        q = one_hot_I @ output_images.permute(0, 2, 3, 1).flatten(0, 2)
-        q = q / (torch.sparse.sum(one_hot_I, 1).to_dense()[:, None] + 1e-6)
+        #each col has only one 1 i.e. each pairing pt (one hot P) and pixel (one hot I) can have only one segment id
+        # However, each row i.e. each segment can have multiple pairing pts and their pixels
+        # k is the segment-wise feature from the point backbone = average features of all pairing pts in this segment 
+        # k = [num segments,  num pairing pts] @ [num pairing pts, 64=feature dim]
+        # output_points is voxel-wise features (num voxels, feature dim) -> output_points[pairing_points contains voxel index for each pairing pt] gives pairing point-wise features 
+        k = one_hot_P @ output_points[pairing_points] #gives sum of all pairing pt features in a segment i.e. [num segments, 64]
+        k = k / (torch.sparse.sum(one_hot_P, 1).to_dense()[:, None] + 1e-6) #[num segments=24*150=3600, 64] / [num segments, 1]
 
-        mask = torch.where(k[:, 0] != 0)
+
+        # q = segment-wise output image features = avg features of all pixels in the segment
+        # q =  [num segments,  num all pixels] @ [num all pixels, 64=feature dim] -> sums pixel-wise features across all pixels in a segment to get segment wise features
+        q = one_hot_I @ output_images.permute(0, 2, 3, 1).flatten(0, 2) # q = [num segments, 64]
+        q = q / (torch.sparse.sum(one_hot_I, 1).to_dense()[:, None] + 1e-6) # #[num segments, 64] / [num segments, 1] i.e. divide by num pixels in a segment to get avg segment feature from the image backbone
+
+        mask = torch.where(k[:, 0] != 0) # since k has features for 150 segments in each image, not all segment ids are present in an img so their feature vectors will be zero. Hence, select non-zero segment features
         k = k[mask]
         q = q[mask]
 
         return self.criterion(k, q)
 
     def training_epoch_end(self, outputs):
-        self.epoch += 1
-        if self.epoch == self.num_epochs:
+        # self.epoch += 1
+        if self.current_epoch == self.num_epochs-1:
             self.save()
         return super().training_epoch_end(outputs)
 
@@ -155,12 +165,12 @@ class LightningPretrain(pl.LightningModule):
 
     @rank_zero_only
     def save(self):
-        path = os.path.join(self.working_dir, "model.pt")
+        path = os.path.join(self._config['savedir_root'], "model.pt")
         torch.save(
             {
                 "model_points": self.model_points.state_dict(),
                 "model_images": self.model_images.state_dict(),
-                "epoch": self.epoch,
+                "epoch": self.current_epoch,
                 "config": self._config,
             },
             path,
