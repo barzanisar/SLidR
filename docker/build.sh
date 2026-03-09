009a63738265532abbabaf22e003db89420f618f
Barza Nisar <barzanisar93@gmail.com>
2026-03-09 14:07:16 -0400

    last changes

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-docker build -t slidr -f Dockerfile ..
-#docker build -t slidr:torch230 -f Dockerfile_new ..
+#docker build -t slidr -f Dockerfile ..
+docker build -t slidr:torch230 -f Dockerfile_new ..
 
+# sudo SINGULARITY_NOHTTPS=1 singularity build ssl_cluster.sif docker-daemon://ssl:cluster_nuscenes_fixed_1
 # sudo SINGULARITY_NOHTTPS=1 singularity build slidr.sif docker-daemon://slidr:latest
 #sudo SINGULARITY_NOHTTPS=1 singularity build slidr_torch230.sif docker-daemon://slidr:torch230
 
